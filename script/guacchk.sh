c2862bdc24d95fabaf6420ee05cd1098e8e4a51e
johnpatrick23 <johnpatrickpapares@gmail.com>
2021-06-28 02:32:03 +0000

    Fix Spam email

--- script/guacchk.sh
+++ script/guacchk.sh
@@ -9,11 +9,15 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 source ./../script/parse_yaml.sh
 
-log=./../logs/madeline.log
+log=madeline.log
 
 sendemail=false
 manageable=true
-restartAttempt=1
+restartAttempt=0
+guacd=1
+mysql=1
+nginx=1
+tomcat8=1
 
 
 for (( ;; ))
@@ -24,33 +28,41 @@ do
 	then
 		echo "[$(date)]: Guac is NOT running" >> $log
 		sendemail=true
+		guacd=0
 	else
 		echo "[$(date)]: Guac is running" >> $log
+		guacd=1
 	fi
 
 	if [ -z "$(top -b -n 1 | egrep 'mysqld')" ]
 	then
                 echo "[$(date)]: mysqld is NOT running" >> $log
                 sendemail=true
+		mysqld=0
         else
                 echo "[$(date)]: mysqld is running" >> $log
-        fi
+		mysqld=1
+	fi
 
 	if [ -z "$(top -b -n 1 | egrep 'nginx')" ]
 	then
                 echo "[$(date)]: nginx is NOT running" >> $log
                 sendemail=true
+		nginx=0
         else
                 echo "[$(date)]: nginx is running" >> $log
-        fi
+        	nginx=1
+	fi
 
 	if [ -z "$(top -b -n 1 | egrep 'tomcat8')" ]
 	then
                 echo "[$(date)]: tomcat8 is NOT running" >> $log
                 sendemail=true
+		tomcat8=0
         else
                 echo "[$(date)]: tomcat8 is running" >> $log
-        fi
+        	tomcat8=1
+	fi
 
 	echo "" >> $log
 	echo "[$(date)]: ------{ guacd }-------------" >> $log
@@ -68,6 +80,15 @@ do
         echo "[$(date)]: ------{ tomcat8 }-----------" >> $log
 	echo $(sudo service tomcat8 status) >> $log 2>&1
 
+	echo "restartAttempt $restartAttempt"
+
+	if [[ "$guacd" = 1 && "$mysql" = 1 && "$nginx" = 1 && "$tomcat8" = 1 ]]
+        then
+		restartAttempt=0
+		sendemail=false
+        else
+		sendemail=true
+        fi
 
 	if [ "$sendemail" = true ] && [ "$manageable" = true ]
 	then
@@ -75,26 +96,26 @@ do
 		sudo service mysql restart || true
 		sudo service nginx restart || true
 		sudo service tomcat8 restart || true
-		echo "[$(date)]: Restart Attempt " $(( restartAttempt++ )) >> $log
-		sendemail=false
-	else
-		restartAttempt=1
-		sendemail=false
-		manageable=true
-	fi
 
-	if [ $restartAttempt = 5 ]
-	then
-		manageable=false
-                if [ "$sendemail" = true ] && [ "$manageable" = false ]
-                then
-			message="Something went wrong in guac server: $(hostname)<br/><br/>After $restartAttempt attempt(s) Guac Service is still not running.<br/>Please check the server ASAP.<br/><br/>"
-                        #sudo systemctl restart guacd
-                        status="<br/>GUAC----<br/>"$(sudo systemctl status guacd || true)"<br/><br/><br/>MYSQL----<br/>"$(sudo systemctl status mysql || true)"<br/><br/><br/>NGINX----<br/>"$(sudo systemctl status nginx || true)"<br/><br/><br/>TOMCAT8----<br/>"$(sudo systemctl status tomcat8 || true)"<br/><br/>"
-                        #echo $message$status
-                        sudo python sendemail.py -m "$message$status" -s "Guac Report"
-                fi
+		if [ $restartAttempt = 5 ]
+		then
+			manageable=false
+			if [ "$sendemail" = true ] && [ "$manageable" = false ]
+	                then
+	                        message="Something went wrong in guac server: $(hostname)<br/><br/>After $restartAttempt attempt(s) Guac Service is still not running.<br/>Please check the server ASAP.<br/><br/>"
+	                        status="<br/>GUAC----<br/>"$(sudo systemctl status guacd || true)"<br/><br/><br/>MYSQL----<br/>"$(sudo systemctl status mysql || true)"<br/><br/><br/>NGINX----<br/>"$(sudo systemctl status nginx || true)"<br/><br/><br/>TOMCAT8----<br/>"$(sudo systemctl status tomcat8 || true)"<br/><br/>"
+	                        sudo python sendemail.py -m "$message$status" -s "Guac Report"
+			fi
+		elif [ $restartAttempt -gt 5 ]
+		then
+			sendemail=false
+			manageable=false
+		else
+			sendemail=true
+			manageable=true
+		fi
 	fi
+	echo "[$(date)]: Restart Attempt " $(( restartAttempt++ )) >> $log
 
 	sleep 5s
 done
